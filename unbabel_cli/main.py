271a125bf72e16b634f8cd73002daf771f2d0299
Daniel Hangan <hangandaniel1@gmail.com>
2024-02-06 12:10:06 +0000

    new refactored moving_average

--- unbabel_cli/main.py
+++ unbabel_cli/main.py
@@ -4,11 +4,11 @@ import json
 import click
 from .data_import.events_reader import process_events_json
 from .data_validation.events_validator import events_input_validator, events_output_validator
-from .data_processing.moving_average import aggregate_translation_delivered
+from .data_processing.moving_average import calculate_moving_average
 
 @click.command()
 @click.option('--input_file', default='events.json', type=str, help='Input file name.')
-@click.option('--window_size', default=10, type=click.IntRange(10, 25), help='Window size for the moving average (between 10 and 25).')
+@click.option('--window_size', default=10, type=click.IntRange(5, 25), help='Window size for the moving average (between 10 and 25).')
 def main(input_file, window_size):
     """
     Main function that reads the input file and prints the moving average of 'translation_delivered' events.
@@ -21,8 +21,7 @@ def main(input_file, window_size):
     events_input_validator(data)
 
     # Calculate the moving average
-    result = aggregate_translation_delivered(data, window_size)
-    
+    result = calculate_moving_average(data, window_size)
     # validate return output
     events_output_validator(result)
     
